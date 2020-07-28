acef43a0fc480d8e9a4027c97429db8d5f791995
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-28 00:19:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,22 @@ attr_accessor :name, :artist, :genre
     @@genre.each do |genre|
       if genre_count[genre]
         genre_count[genre] +=1 
-      else genre_count[genre] = 1 
+      else 
+        genre_count[genre] = 1 
     end
   end
   genre_count
 end
+
+def self.artist_count
+  artist_count = {}
+  @@artist.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] +=1 
+    else
+      artist_count[artist] = 1
+    end
+  end
+  artist_count
+end
+
